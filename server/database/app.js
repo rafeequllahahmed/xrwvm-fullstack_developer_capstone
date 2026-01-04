1d81d4e8dcf547d13deb224c0576d968f5a60046
rafeequllahahmed <rafeequllahahmed@gmail.com>
2026-01-04 17:46:36 -0500

    app.js fixed

--- server/database/app.js
+++ server/database/app.js
@@ -1,15 +1,23 @@
 /*jshint esversion: 8 */
+/*jshint esversion: 8 */
 const express = require('express');
+
 const mongoose = require('mongoose');
+
 const fs = require('fs');
+
 const  cors = require('cors')
+
 const app = express()
+
 const port = 3030;
 
 app.use(cors())
+
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
+
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
@@ -21,41 +29,65 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
+
     Reviews.insertMany(reviews_data['reviews']);
+
   });
+
   Dealerships.deleteMany({}).then(()=>{
+
     Dealerships.insertMany(dealerships_data['dealerships']);
+
   });
   
 } catch (error) {
+
   res.status(500).json({ error: 'Error fetching documents' });
 }
 
 
 // Express route to home
+
 app.get('/', async (req, res) => {
+
     res.send("Welcome to the Mongoose API")
 });
 
 // Express route to fetch all reviews
+
 app.get('/fetchReviews', async (req, res) => {
+
   try {
+
     const documents = await Reviews.find();
+
     res.json(documents);
+
   } catch (error) {
+
     res.status(500).json({ error: 'Error fetching documents' });
   }
 });
 
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
+
   try {
+
     const documents = await Reviews.find({dealership: req.params.id});
+
     res.json(documents);
+
   } catch (error) {
+
     res.status(500).json({ error: 'Error fetching documents' });
+
   }
+
 });
+
+
+
 
 
 
