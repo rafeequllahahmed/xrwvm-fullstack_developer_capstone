56e157e6885fb94a6c936117a56414a8185884d2
rafeequllahahmed <rafeequllahahmed@gmail.com>
2026-01-05 16:28:37 -0500

    Fix syntax and indentation errors

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,9 +1,5 @@
-# Uncomment the imports below before you add the function code
-
 import requests
-
 import os
-
 from dotenv import load_dotenv
 
 # Load environment variables first
@@ -12,71 +8,49 @@ load_dotenv()
 backend_url = os.getenv("backend_url", default="http://localhost:3030")
 
 sentiment_analyzer_url = os.getenv(
-    "sentiment_analyzer_url", default="http://localhost:5050/"
+    "sentiment_analyzer_url",
+    default="http://localhost:5050/"
 )
 
 
 def get_request(endpoint, **kwargs):
-
     params = ""
-
     if kwargs:
-
         for key, value in kwargs.items():
-
             params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
-
     print("GET from {} ".format(request_url))
 
-try:
+    try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
-
         return response.json()
-
     except Exception:
         print("Network exception occurred")
         return None
 
 
 def analyze_review_sentiments(text):
-
     request_url = sentiment_analyzer_url + "analyze/" + text
 
     try:
-
         # Call get method of requests library with URL and parameters
-
         response = requests.get(request_url)
-
         return response.json()
-
     except Exception as err:
-
         print(f"Unexpected {err=}, {type(err)=}")
-
         print("Network exception occurred")
-
         return {"sentiment": "neutral"}
 
 
 def post_review(data_dict):
-
     request_url = backend_url + "/insert_review"
 
     try:
         response = requests.post(request_url, json=data_dict)
-
         return response.json()
-
     except Exception as err:
-
         print(f"Unexpected {err=}, {type(err)=}")
-
         print("Network exception occurred")
-
         return None
-
-        # code end here
